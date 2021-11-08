5357da577a3cd9fad2dd7f71243af33ac8c788ec
830503 <lixiaoqi19830503@gmail.com>
2021-11-08 21:18:40 +0100

    added back Database class

---
+++ Model/Database.php
@@ -0,0 +1,19 @@
+<?php
+//database connection----need to be modify!!!
+class Database
+{
+    private $dbServer;
+    Private $dbUser;
+    Private $dbPassword;
+    Private $dbName;
+
+    public function dataConnection()
+    {
+        $this->dbServer = "becode.localhost";
+        $this->dbUser = "root";
+        $this->dbPassword = "admin";
+        $this->dbName = "priceCalculator";
+        $connection = new mysqli($this->dbServer, $this->dbUser, $this->dbPassword, $this->dbName);
+        return $connection;
+    }
+}
--- index.php
+++ index.php
@@ -10,22 +10,4 @@ require 'Model/Product.php';
 require 'Model/ProductLoader.php';
 require 'Controller/HomepageController.php';
 
-<?php
-//database connection----need to be modify!!!
-class Database
-{
-    private $dbServer;
-    Private $dbUser;
-    Private $dbPassword;
-    Private $dbName;
 
-    public function dataConnection()
-    {
-        $this->dbServer = "becode.localhost";
-        $this->dbUser = "root";
-        $this->dbPassword = "admin";
-        $this->dbName = "priceCalculator";
-        $connection = new mysqli($this->dbServer, $this->dbUser, $this->dbPassword, $this->dbName);
-        return $connection;
-    }
-}

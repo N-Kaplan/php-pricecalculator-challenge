a8d5c04349fb3a1bc26f534a3a0a08e3e4ca9ac7
Naama Kaplan <nkaplany@gmail.com>
2021-11-09 14:13:44 +0100

    method to find the largest variable discount functional

--- Model/Calculator.php
+++ Model/Calculator.php
@@ -2,5 +2,39 @@
 
 class Calculator
 {
+    private Product $product;
+    private Customer $customer;
+    private CustomerGroup $customerGroup;
 
+    /**
+     * @param Product $product
+     * @param Customer $customer
+     * @param CustomerGroup $customerGroup
+     */
+    public function __construct(Product $product, Customer $customer, CustomerGroup $customerGroup)
+    {
+        $this->product = $product;
+        $this->customer = $customer;
+        $this->customerGroup = $customerGroup;
+    }
+
+    private function pickVariableDiscount()
+    {
+        //add all relevant groups' variable discounts to 1 array
+        $groups_var_discount = [intval($this->customerGroup->getVariableDiscount())];
+        $parent_group_id = $this->customerGroup->getParentId();
+        if ($parent_group_id !== null) {
+            $gl = new CustomerGroupLoader();
+            $parent_group = $gl->getCustomerGroupById($parent_group_id);
+            $groups_var_discount[] = intval($parent_group->getVariableDiscount());
+
+            //check if parent group has its own parent group
+            $grandparent_group_id = $parent_group->getParentId();
+            if ($grandparent_group_id !== null) {
+                $grandparent_group = $gl->getCustomerGroupById($grandparent_group_id);
+                $groups_var_discount[] = intval($grandparent_group->getVariableDiscount());
+            }
+        }
+        return max($groups_var_discount);
+    }
 }

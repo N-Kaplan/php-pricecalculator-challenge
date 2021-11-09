c9916e1f36d1d478a6d13731b6039776dc553d1e
Naama Kaplan <nkaplany@gmail.com>
2021-11-09 10:41:46 +0100

    correction

--- index.php
+++ index.php
@@ -30,7 +30,6 @@ $result =  $db->dataConnection()->query($sql);
 //
 //var_dump($products);
 
-<<<<<<< HEAD
 $pl = new ProductLoader();
 $all_products = $pl->getProducts();
 //var_dump($all_products);
@@ -43,19 +42,3 @@ echo $test;
 $cl = new CustomerLoader();
 $all_customers = $cl->getCustomers();
 //var_dump($all_customers);
-=======
-/*$sql = "SELECT * FROM product";
-$db =  new Database;
-$result =  $db->dataConnection()->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while ($row = $result->fetch_assoc()) {
-        var_dump($row);
-        $row->
-    }
-}*/
-
-$customerLoader = new CustomerLoader;
-$cust = $customerLoader->getCustomers();
->>>>>>> 9a009b7c722c3138c6616ff1d611a52389f2d3d0

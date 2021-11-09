0252e71e625879de3d57b68de275fbaf6bbad875
830503 <lixiaoqi19830503@gmail.com>
2021-11-09 10:39:27 +0100

    fixing problems

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 
-//require 'Model/Database.php';
+require 'Model/Database.php';
 require 'Model/Customer.php';
 require 'Model/CustomerLoader.php';
 require 'Model/CustomerGroup.php';
@@ -11,5 +11,26 @@ require 'Model/ProductLoader.php';
 require 'Controller/HomepageController.php';
 
 
+/*$sql = "SELECT * FROM product";
+$db =  new Database;
+$result =  $db->dataConnection()->query($sql);
 
+if ($result->num_rows > 0) {
+    // output data of each row
+    while ($row = $result->fetch_assoc()) {
+        var_dump($row);
+        $row->
+    }
+}*/
+$pl = new ProductLoader();
+$all_products = $pl->getProducts();
+//var_dump($all_products);
 
+$gl = new CustomerGroupLoader();
+$all_groups = $gl->getCustomerGroups();
+$test = $all_groups[0]->getId();
+echo $test;
+
+$cl = new CustomerLoader();
+$all_customers = $cl->getCustomers();
+//var_dump($all_customers);
